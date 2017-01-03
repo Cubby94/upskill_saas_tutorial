a8256288b711a9d2c441111922e6cd06525f99e5
Caleb Potter <calebpotter94@gmail.com>
2017-01-03 21:40:37 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,10 @@ class UsersController < ApplicationController
   
   # GET /users
   def index
+    results_per_page = 2
+    page = Float(params[:page])
     
+    @users = User.find(page * results_per_page + 1, page * results_per_page + 2)
   end
   
   
